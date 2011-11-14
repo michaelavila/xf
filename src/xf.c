2cd6577bf23dbd8978f958d7a662927ff0c35782
t3hh00d <t3hh00d@gmail.com>
2011-11-13 17:26:20 -0800

    added init command

--- src/xf.c
+++ src/xf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 #include "help.h"
+
+int does_xfiles_exist();
 
 int main(int argc, char *argv[]) {
     // no command specified, show help
@@ -12,13 +15,27 @@ int main(int argc, char *argv[]) {
 
     const char *cmd = argv[1];
 
-    // handle command
+    // handle help and version commands
     if (!strcmp(cmd, "help")) {
         show_usage();
         show_commands();
+        return 0;
     } else if (!strcmp(cmd, "version")) {
         show_version();
+        return 0;
+    }
+    
+    if (!xfiles_exists() && !strcmp(cmd, "init")) {
+        mkdir(".xfiles", S_IRUSR | S_IWUSR);
+        return 0;
     }
 
+    // handle commands that require .xfiles
+
     return 0;
+}
+
+int xfiles_exists() {
+    struct stat sb;
+    return stat(".xfiles", &sb) == 0 && S_ISDIR(sb.st_mode);
 }
